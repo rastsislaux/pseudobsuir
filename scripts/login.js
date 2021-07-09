519fae840c58259b31bd993a0e56d94d46fccc6d
ungaf <ungaf@ya.ru>
2021-07-09 23:21:32 +0300

    Created site

---
+++ scripts/login.js
@@ -0,0 +1,6 @@
+window.onload = init;
+
+function init() {
+    var name_link = document.getElementById("namelink");
+    name_link.innerHTML = "Иван Иванович";
+}
---
+++ scripts/tabs.js
@@ -0,0 +1,8 @@
+window.onload = init;
+
+function init() {
+    $('#myTab a').on('click', function (e) {
+        e.preventDefault()
+        $(this).tab('show')
+      })
+}
